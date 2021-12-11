04b2b034186d3d8194412d58b0ade9acb6d412f4
StoneShuyao <ss119@ie.cuhk.edu.hk>
2021-12-11 20:15:38 +0800

    update parser and model

--- src/tasks/semantic/dataset/kitti/parser.py
+++ src/tasks/semantic/dataset/kitti/parser.py
@@ -87,8 +87,8 @@ class SemanticKitti(Dataset):
       #scan_path = os.path.join(self.root, seq, "velodyne")
       #label_path = os.path.join(self.root, seq, "labels")
 
-      scan_path = self.root
-      label_path = self.root
+      scan_path = os.path.join(self.root, seq)
+      label_path = os.path.join(self.root, seq)
 
       # get files
       scan_files = [os.path.join(dp, f) for dp, dn, fn in os.walk(
